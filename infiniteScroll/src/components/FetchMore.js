3fd4b94d3db97d1fc7307a4fc91d6f24c6b885e2
kimzerovirus <yg9699k@naver.com>
2022-01-18 10:03:37 +0900

    study intersectionObserver

---
+++ infiniteScroll/src/components/FetchMore.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from "react";
+
+const FetchMore = ({ loading, setPage }) => {
+  const fetchMoreTrigger = useRef(null);
+  const fetchMoreObserver = new IntersectionObserver((entries) => {
+    console.log(entries);
+    console.log(entries[0]);
+    // if (entries[0].isIntersecting) setPage((page) => page + 1);
+    if (entries[0].intersectionRatio <= 0) return;
+    setPage((page) => page + 1);
+  });
+
+  useEffect(() => {
+    fetchMoreObserver.observe(fetchMoreTrigger.current);
+    return () => {
+      fetchMoreObserver.unobserve(fetchMoreTrigger.current);
+    };
+  }, []);
+
+  return (
+    <div
+      id="fetchMore"
+      className={loading ? "loading" : ""}
+      ref={fetchMoreTrigger}
+    />
+  );
+};
+
+export default FetchMore;

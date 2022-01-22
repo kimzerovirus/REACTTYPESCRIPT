c25833592b9e9d590d65b921659b16ca84f01604
kimzerovirus <yg9699k@naver.com>
2022-01-22 22:10:02 +0900

    add description of toolkit

--- reduxtoolkit-basic/src/app/store.ts
+++ reduxtoolkit-basic/src/app/store.ts
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import reservationsReducer from "../features/reservationSlice";
 import customerReducer from "../features/customerSlice";
 
+// createStore를 대체해서 configureStore() 사용하면, Thunk, dev tool까지 자동으로 연결해준다.
+//property명은 반드시 reducer로 시작한다.
 export const store = configureStore({
   reducer: {
-    reservations: reservationsReducer,
+    reservations: reservationsReducer, //export 할 때 리듀서 모양 그대로 보냈기 때문에 바로 넣어줄 수 있다.
     customer: customerReducer,
   },
 });
--- reduxtoolkit-basic/src/features/reservationSlice.ts
+++ reduxtoolkit-basic/src/features/reservationSlice.ts
@@ -1,27 +1,37 @@
-import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface ReservationState{
-    value: string[]
+interface ReservationState {
+  value: string[];
 }
 
-const initialState:ReservationState = {
-    value:["Yuri"]
-}
+const initialState: ReservationState = {
+  value: ["Yuri"],
+};
 
 export const reservationsSlice = createSlice({
-    name:'reservations',
-    initialState,
-    reducers:{
-        addReservation: (state, action:PayloadAction<string>) => {
-            state.value.push(action.payload)
-        },
-        removeReservation: (state, action: PayloadAction<number>)=>{
-            state.value.splice(action.payload, 1)
-        }
+  name: "reservations",
+  initialState,
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+    removeReservation: (state, action: PayloadAction<number>) => {
+      state.value.splice(action.payload, 1);
+    },
+  },
+});
 
-    }
-})
+export const { addReservation, removeReservation } = reservationsSlice.actions;
 
-export const {addReservation,removeReservation} = reservationsSlice.actions
-
+//slice를 reservationsSlice.reducer로 export default하면 reducer 모양으로 그대로 스토어에서 받을 수 있다.
 export default reservationsSlice.reducer;
+
+/*
+    리듀서 함수를 구성할 때
+    위에서는 직접 값을 변경하는 방법으로 구성하였는데
+    만약 기존값을 복사하여 새로운 값을 만들어서 state에 저장한다면
+    addReservation: (state, action: PayloadAction<string>) => {
+      return { ...state, value:action.payload }
+    },
+    다음과 같이 전개연산자로 기존 값을 꺼낸 후 합쳐서 리턴해줘야 한다.
+*/
